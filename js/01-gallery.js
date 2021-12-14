157825e8ba1d00d8c5254085d1f2f1de33aee213
Alona Puhach <alen1414@gmail.com>
2021-12-14 11:52:35 -0400

    images/add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const paletteContainer = document.querySelector('.gallery');
+const cardsMarkup = createCardsMarkup(galleryItems);
+    paletteContainer.insertAdjacentHTML('beforeend',cardsMarkup)
+console.log(createCardsMarkup(galleryItems));
+
+function createCardsMarkup(cards) {
+  return galleryItems.map(({ preview, original, description }) => {
+      return `
+   <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+    })
+    .join('');
+}
+
+
+
+
+    
+
+
+
+
+// const cardsMarkup = createGalaryCards(cards);
+// console.log(cardsMarkup);
+
+// galeryEl.insertAdjacentElement('beforeend', cardsMarkup);
+
+// galeryEl.addEventListener('click')
+// const galeryEl = document.querySelector('.gallery')
+// console.log(galeryEl)
+
+// const galeryItemEl = document.querySelector('.gallery__item')
+// console.log(galeryItemEl);
+
+// const galeryItemLinkEl = document.querySelector('.gallery__link')
+// console.log(galeryItemLinkEl);
